5f9cc4faf5e12713318e2554995ab5310909e502
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2024-12-21 21:45:05 +0330

    setup app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,26 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        screens: {
+            tablet: "640px",
+            laptop: "1024px",
+            desktop: "1280px",
+        },
+        extend: {
+            fontFamily: {
+                sans: ["var(--font-vazir)", ...fontFamily.sans],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light", "dark", "aqua"],
+    },
 } satisfies Config;
