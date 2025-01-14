dd2f25cf9915143bf4067e812efe23611f059060
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-01-14 17:06:14 +0330

    set metadata

---
+++ src/app/blogs/[[...slug]]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "صفحه بلاگ ها",
+};
+
+export default function page() {
+    return <h1>Blog page</h1>;
+}
